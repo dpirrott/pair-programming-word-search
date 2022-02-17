8bcbb247df578facfac087f271da152ad2bb641b
Dylan Pirrotta <dpirrotta9@hotmail.com>
2022-02-17 01:47:24 +0000

    Completed wordsearch pair programming

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,36 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  const result = [];
+  for (let i = 0; i < matrix[0].length; i++) {
+    result.push([]);
+    for (let j = 0; j < matrix.length; j++) {
+      result[i].push(matrix[j][i]);
     }
-}
+  }
+  return result;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  
+  const horizontalJoin = letters.map(ls => ls.join(''));
+
+  const verticalJoin = transpose(letters).map(ls => ls.join(''));
+
+  console.log(verticalJoin[0]);
+
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  for (let h of verticalJoin) {
+    if (h.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
+
+/*Feb 16, 2022
+Pair Programming - Word Search
+Feb 7 cohort: Madiha, Dylan
+*/
